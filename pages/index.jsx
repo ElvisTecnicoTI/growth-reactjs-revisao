8866d4961f96c5720c29181acc9454ffabc98dbc
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2023-04-15 17:29:49 -0300

    criei um exemplo com sass

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,13 @@
 import Title from '../src/components/title/title'
+import Button from '../src/components/button/button'
 
 export default function HomePage() {
-  return <Title>Título da página</Title>
+  return (
+    <>
+      <Title>Título da página</Title>
+      <Button>Botão</Button>
+      <label>Label Teste</label>
+      <button>Button</button>
+    </>
+  )
 }
