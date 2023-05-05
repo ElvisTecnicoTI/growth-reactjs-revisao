537407d46948fd7075e4b85c2024545960ccef45
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2023-05-04 21:58:57 -0300

    configurei o style components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
 
 export default function HomePage() {
   return (
     <>
-      <Title>Título da página</Title>
-      <Button>Botão</Button>
+      <Title>Formulário de Login</Title>
+      <label>Voltar a pagina...</label>
+      <form>
+        <Input type="email" placeholder="Digite o seu email" />
+        <Input type="password" placeholder="Digite a sua senha"/>
+        <Button>Botão</Button>
+
+      </form>
+    
       <label>Label Teste</label>
       <button>Button</button>
     </>
