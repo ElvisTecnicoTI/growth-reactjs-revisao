0585999bf66f57963a95dd75d6104882747cce5f
elvis_tecnico_ti <elvistecnico_ti@protonmail.com>
2023-05-12 23:31:30 -0300

    criei os estados dos compontentes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react'
+
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
 
 export default function HomePage() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleLabelClick = () => {
     console.log('HANDLE LABEL CLICK')
@@ -11,22 +16,29 @@ export default function HomePage() {
   const handleSubmitForm = (event) => {
     event.preventDefault()
     console.log('HANDLE SUBMIT FORM')
+    console.log({ email, password })
   }
-
-  let email = ''
-  const handleChangeInput = (event) => {
-    email = event.target.value
+  
+  const handleChangeInput = (event, onChange) => {
+    onChange(event.target.value)
   }
-
 
   return (
     <>
       <Title onClick={handleLabelClick}>Formulário de Login</Title>
       <label onClick={handleLabelClick}>Voltar a pagina...</label>
       <form onSubmit={handleSubmitForm}>
-        <Input type="text" placeholder="Digite o seu email" onChange={handleChangeInput} />
+        <Input 
+          type="text" 
+          placeholder="Digite o seu email" 
+          onChange={(e) => handleChangeInput(e, setEmail)} 
+        />
         <p>O email digitado foi: {email}</p>
-        <Input type="password" placeholder="Digite a sua senha" onChange={handleChangeInput}/>
+        <Input 
+          type="password" 
+          placeholder="Digite a sua senha" 
+          onChange={(e) => handleChangeInput(e, setPassword)}
+        />
         <Button type="submit">Botão form</Button>
 
       </form>
