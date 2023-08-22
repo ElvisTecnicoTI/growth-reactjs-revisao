be3bd6b6b4d26f1d17df354b09fad56168c21170
ElvisTecnicoTI <elvis_tecnico_ti@outlook.com>
2023-08-21 21:31:52 -0300

    criei um exemplo de useEffect

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react'
+
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
 
 export default function HomePage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const handleSubmitForm = (event) => {
         event.preventDefault()
         console.log("Handle Submit Form")
+        console.log({email, password})
     }
     
-    let email = ''
-    const handleChangeInput = (event) => {
-        email = event.target.value
+    const handleChangeInput = (event, onChange) => {
+        onChange(event.target.value)
     }
 
     return(
@@ -19,9 +23,9 @@ export default function HomePage() {
          <Title>LOGIN</Title>
          <label>Voltar a página...</label>
          <form onSubmit={handleSubmitForm}>
-            <Input type="email" placeholder="Digite seu e-mail" onChange={handleChangeInput} />
+            <Input type="email" placeholder="Digite seu e-mail" onChange={(e) => handleChangeInput(e, setEmail)} />
             <p>O e-mail digita foi: {email}</p>
-            <Input type="password" placeholder="Digite a sua senha" onChange={handleChangeInput} />
+            <Input type="password" placeholder="Digite a sua senha" onChange={(e) => handleChangeInput(e, setPassword)} />
             <Button type="submit">Login</Button>
          </form>
          <label>LABEL TESTE</label>
